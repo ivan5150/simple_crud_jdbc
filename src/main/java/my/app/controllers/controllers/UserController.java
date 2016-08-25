29bb020ff3be72c6f2c043c1816d599e7511e112
ivan5150 <rat90@inbox.ru>
2016-08-25 17:24:41 +0300

    + @Autowired  private UserService userService;

--- src/main/java/my/app/controllers/controllers/UserController.java
+++ src/main/java/my/app/controllers/controllers/UserController.java
@@ -3,6 +3,7 @@ package my.app.controllers.controllers;
 import my.app.controllers.dao.UserDao;
 import my.app.controllers.dao.UserDaoJdbcImpl;
 import my.app.controllers.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.ModelMap;
@@ -20,6 +21,9 @@ public class UserController {
     public static final String ALL_USERS_PAGE = "AllUsers";
     public static final String ADD_USER_PAGE = "AddUser";
     ArrayList<User> list = new ArrayList<User>();
+    @Autowired
+    private UserService userService;
+
 
 
     @RequestMapping(value = "/add/user", method = RequestMethod.GET)
@@ -37,8 +41,9 @@ public class UserController {
     @RequestMapping(value = "/save/user", method = RequestMethod.POST)
     public String saveMessage(ModelMap model) {
         model.addAttribute("tbl", UserDaoJdbcImpl.INSERT_USER);
-     //   model.addAttribute("users", userService.getAll());
-     //   model.addAttribute("user", new UserServiceImpl().insert());
+        model.addAttribute("users", userService.getAll());
+       // model.addAttribute("users", userService.insert(User user));
+
 
         return showPageAllUsers(model);
     }
